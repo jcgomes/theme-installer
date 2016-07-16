5d61be9d1d58c2295e7c74de0860da28ecd9e573
Juliano C. Gomes <jcgomes@users.noreply.github.com>
2016-07-16 13:43:42 -0300

    Add files via upload

---
+++ theme-installer/mainwindow.cpp
@@ -0,0 +1,55 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pbInstall_clicked()
+{
+    int index;
+    index = ui->cbChoose->currentIndex(); // 0=icons; 1=theme
+
+    QMessageBox msgBox;
+    if (index==0) {
+        QString file = QFileDialog::getOpenFileName(this, tr("Select an file"),"/home", tr("Compressed Files (*.tar.gz *.tgz)"));
+
+        if (file != ""){
+            std::string str = "tar -zxvf " + file.toStdString() + " -C /usr/share/icons/";
+            system(str.c_str());
+            msgBox.setText("All done, you can now close the application and change your icons.");
+            msgBox.exec();
+        }
+        else
+        {
+            msgBox.setText("You have to choose a '.tar.gz' or '.tgz' file.");
+            msgBox.exec();
+        }
+    }
+    else if (index==1) {
+        QString file = QFileDialog::getOpenFileName(this, tr("Select an file"),"/home", tr("Compressed Files (*.tar.gz *.tgz)"));
+
+        if (file != ""){
+            std::string str = "tar -zxvf "+file.toStdString()+" -C /usr/share/themes/";
+            system(str.c_str());
+            msgBox.setText("All done, you can now close the application and change your theme.");
+            msgBox.exec();
+        }
+        else
+        {
+            msgBox.setText("You have to choose a '.tar.gz' or '.tgz' file.");
+            msgBox.exec();
+        }
+    }
+
+}
---
+++ theme-installer/theme-installer.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-07-15T02:01:57
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = theme-installer
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
